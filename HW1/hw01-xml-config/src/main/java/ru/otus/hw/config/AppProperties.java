b1bb34884639f3ce22dd0ddccad2dec31f3fe0ea
i.kineev <i.kineev@argustelecom.ru>
2024-12-24 17:06:13 +0300

    hw1.1

--- HW1/hw01-xml-config/src/main/java/ru/otus/hw/config/AppProperties.java
+++ HW1/hw01-xml-config/src/main/java/ru/otus/hw/config/AppProperties.java
@@ -1,9 +1,7 @@
 package ru.otus.hw.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
 @Data
 public class AppProperties implements TestFileNameProvider {
     private String testFileName;
