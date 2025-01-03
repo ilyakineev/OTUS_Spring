3eab5746e699736c4d225a9b5102c85bc797f7bd
iKineev <kineev.ilya@gmail.com>
2025-01-03 23:02:35 +0300

    hw3.2

--- HW3/hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ HW3/hw03-spring-boot/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,25 +1,46 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.config.AppProperties;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
-import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 @RequiredArgsConstructor
 @Component
 public class CsvQuestionDao implements QuestionDao {
-    private final TestFileNameProvider fileNameProvider;
+    private final AppProperties fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        String fileName = fileNameProvider.getTestFileName();
 
-        return new ArrayList<>();
+        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName)) {
+            if (inputStream == null) {
+                throw new QuestionReadException("CSV файл не найден: " + fileName);
+            }
+
+            try (InputStreamReader reader = new InputStreamReader(inputStream)) {
+                CsvToBean<QuestionDto> csvToBean = new CsvToBeanBuilder<QuestionDto>(reader)
+                        .withType(QuestionDto.class)
+                        .withSeparator(';')
+                        .withSkipLines(1)
+                        .build();
+
+                return csvToBean.parse()
+                        .stream()
+                        .map(QuestionDto::toDomainObject)
+                        .toList();
+            }
+        } catch (Exception e) {
+            throw new QuestionReadException("Ошибка при чтении CSV файла", e);
+        }
     }
 }
